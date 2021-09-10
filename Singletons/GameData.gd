69b33b7a17d8f3de333bd4d8a721344a19fedebd
Allan Galera <allangalera@gmail.com>
2021-09-09 23:33:26 -0300

    videos 3 4 e 5

---
+++ Singletons/GameData.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var tower_data = {
+	"GunT1": {
+		"damage": 20,
+		"rof": 1,
+		"range": 350
+	},
+	"GunT2": {
+		"damage": 50,
+		"rof": 1,
+		"range": 350
+	},
+	"MissleT1": {
+		"damage": 100,
+		"rof": 3,
+		"range": 550
+	}
+}
